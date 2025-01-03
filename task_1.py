6ccb218c146cb0ea43bda624e25fc600a717ff6c
kucheryaska <kucheryaska12@gmail.com>
2025-01-03 20:37:54 +0300

    Задача 2

--- task_1.py
+++ task_1.py
@@ -12,9 +12,22 @@ def cook_book(fail):
         cook_book[q[0]] = []
         while w < int(q[1]):
             p = q[w+2].split('|')
-            ingredient = {'ingredient_name': p[0], 'quantity': p[1], 'measure': p[2]}
+            ingredient = {'ingredient_name': p[0].strip(), 'quantity': p[1].strip(), 'measure': p[2].strip()}
             cook_book[q[0]].append(ingredient)
             w += 1
     return cook_book
 
-print(cook_book('Recepts.txt'))
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    ingredients_list = {}
+    cook_book1 = cook_book('Recepts.txt')
+    for i in dishes:
+        if i in cook_book1:
+            for j in cook_book1[i]:
+                if j['ingredient_name'] not in ingredients_list:
+                    ingredients_list[j['ingredient_name']] = {'measure': j['measure'], 'quantity' : int(j['quantity']) * person_count}
+                else:
+                    ingredients_list[j['ingredient_name']]['quantity'] += int(j['quantity']) * person_count
+    return ingredients_list
+
